2da270299efcd02ec0a70103b124f654897bd044
sooochy <kwojdyla00@gmail.com>
2022-04-12 19:33:12 +0200

    done grid

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecenderyAction, IconButton, Slide } from '@material-ui/'
+
+const List = () => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List
